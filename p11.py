5555c512b8cf4d50fca57f8c997294a0cf9032b5
abhishekr12345 <abhishek_rinwa_24347@pgkmschool.org>
2026-03-05 13:46:54 +0530

    Add files via upload

---
+++ p11.py
@@ -0,0 +1,13 @@
+myvar="Abhishek"
+my_var="Vachan"
+_my_var="chaitanya"
+myVar="ansh"
+MYVAR="shravan"
+myvar2="bhavya"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
